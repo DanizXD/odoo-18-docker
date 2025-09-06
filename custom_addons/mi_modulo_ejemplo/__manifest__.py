9f4406e00df44a9907ad926e00ab2f96440400e0
DanizXD <eldanizprograma@gmail.com>
2025-09-05 22:48:43 -0500

    feat: add custom addons directory and example module

---
+++ custom_addons/mi_modulo_ejemplo/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Mi Módulo Ejemplo',
+    'version': '16.0.1.0.0',
+    'summary': 'Un módulo de ejemplo para Odoo',
+    'author': 'Tu Nombre',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
